e0de5d4b6a8004372136351d8c81f1181cf1e481
BuildTools <unconfigured@null.spigotmc.org>
2022-03-26 21:10:39 +0200

    Adding core constants.

---
+++ MyArtPlace.Core/Constants/MessageConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArtPlace.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string ErrorMessage = "ErrorMessage";
+
+        public const string SuccessMessage = "SuccessMessage";
+
+        public const string WarningMessage = "WarningMessage";
+    }
+}
