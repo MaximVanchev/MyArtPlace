403847d93e9dcb711a8e60fdabcbede8896afbf0
BuildTools <unconfigured@null.spigotmc.org>
2022-03-26 21:58:38 +0200

    Adding service extension,

---
+++ MyArtPlace/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyArtPlace.Data;
+using MyArtPlace.Infrastructure.Data;
+using MyArtPlace.Infrastructure.Data.Repositories;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicatioDbRepository, ApplicatioDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MyArtPlaceContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
