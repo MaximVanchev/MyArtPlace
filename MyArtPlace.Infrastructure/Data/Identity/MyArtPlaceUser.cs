632fbbbbd15abae9f656ef672f657e5d47b9c568
BuildTools <unconfigured@null.spigotmc.org>
2022-03-26 20:53:22 +0200

    Adding Identity.

---
+++ MyArtPlace.Infrastructure/Data/Identity/MyArtPlaceUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyArtPlace.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the MyArtPlaceUser class
+public class MyArtPlaceUser : IdentityUser
+{
+}
+
