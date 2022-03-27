fa1a5e73ea5c75b3248bd0cf1a3edf849328097a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 02:55:54 +0200

    Adding database constants.

---
+++ MyArtPlace.Infrastructure/Data/Constants/DatabaseConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArtPlace.Infrastructure.Data.Constants
+{
+    public static class DatabaseConstants
+    {
+        public const int Guid_Max_Length = 100;
+        public const int Currency_Iso_Length = 3;
+        public const int Shop_Name_Max_Length = 50;
+        public const int Product_Name_Max_Length = 50;
+        public const int Category_Name_Max_Length = 20;
+        public const int Name_Min_Length = 5;
+        public const int Description_Max_Length = 200;
+        public const int Location_Max_Length = 100;
+    }
+}
