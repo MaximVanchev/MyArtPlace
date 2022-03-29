57b376ab18297b8085885555a444c29929c77718
BuildTools <unconfigured@null.spigotmc.org>
2022-03-29 23:51:48 +0300

    Adding ProfileEditViewModel.

---
+++ MyArtPlace.Core/Models/User/ProfileEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArtPlace.Core.Models.User
+{
+    public class ProfileEditViewModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public byte[]? ProfilePicture { get; set; }
+    }
+}
