f17ce769dab72b9d1d9f5ac0f473cf6f1e3a3520
BuildTools <unconfigured@null.spigotmc.org>
2022-04-04 04:02:12 +0300

    Adding custom error mesage model for tostr.

---
+++ MyArtPlace/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyArtPlace.Core.Constants;
+using MyArtPlace.Core.Models.Common;
+
+namespace MyArtPlace.Controllers
+{
+    public class BaseController : Controller
+    {
+        public async Task CheckMessages()
+        {
+            if (MessageViewModel.Message.ContainsKey(MessageConstants.SuccessMessage))
+            {
+                string message = MessageConstants.SuccessMessage;
+                ViewData[message] = MessageViewModel.Message[message];
+                MessageViewModel.Message.Remove(message);
+            }
+            else if (MessageViewModel.Message.ContainsKey(MessageConstants.ErrorMessage))
+            {
+                string message = MessageConstants.ErrorMessage;
+                ViewData[message] = MessageViewModel.Message[message];
+                MessageViewModel.Message.Remove(message);
+            }
+            else if (MessageViewModel.Message.ContainsKey(MessageConstants.WarningMessage))
+            {
+                string message = MessageConstants.WarningMessage;
+                ViewData[message] = MessageViewModel.Message[message];
+                MessageViewModel.Message.Remove(message);
+            }
+        }
+    }
+}
